cd6747d44e8bb511989f8739c47468f44673d860
Daniel Petrolio <daneil2711@gmail.com>
2023-04-02 22:10:24 -0300

    Alterações Função

--- fnDaniPacote.cs
+++ fnDaniPacote.cs
@@ -15,21 +15,30 @@ namespace Company.Function
         [FunctionName("fnDaniPacote")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            ILogger log, [Queue("fila-pedido"),StorageAccount("AzureWebJobsStorage")]
+            ICollector<Pedido> itemPedido)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            var pedido = new Pedido()
+            {
+                NumeroPedido = Guid.NewGuid().ToString("n"),
+                Data = data.datapedido,
+                Item = data.item,
+                Preco = (decimal)data.preco
+            };
 
-            return new OkObjectResult(responseMessage);
+            itemPedido.Add(pedido);
+
+            return new OkObjectResult(pedido);
         }
+    }
+    public class Pedido
+    {
+        public string NumeroPedido {get; set;}
+        public string Data {get; set;}
+        public string Item {get; set;}
+        public decimal Preco {get; set;}
     }
 }
